2bb082ca6c1718726cf3e4f4b239f1348f2798d7
Stackbit Code Editor <projects@stackbit.com>
2021-10-28 16:49:20 +0000

    stepzen_fetch.js: changed

---
+++ src/components/stepzen_fetch.js
@@ -0,0 +1,33 @@
+function Home() {
+  const { loading, error, data } = useQuery(GET_QUERY);
+  console.log(data)
+
+  if (loading) return <p>Loading ...</p>;
+
+  if (error) return <pre>ERROR: {JSON.stringify(error, null, 2)}</pre>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={StepZenLogo} alt="StepZen Logo" width="300px" />
+        <h2 style={{ marginTop: "40px" }}>Latest launches information pulled from StepZen Endpoint:</h2>
+        <CardList>
+          {data.launchesPast.map(launch => (
+            <Card key={launch.id}>
+              <b className="Title">{launch.mission_name}</b>
+              <br />
+              <p className="Description">
+                Launched from {launch.launch_site.site_name}. <br />
+                Used rocket {launch.rocket.rocket_name}
+              </p>
+            </Card>
+          ))}
+        </CardList>
+
+        <Link to="/rockets" role="button" className="RocketsInformation">
+          See rockets information...
+        </Link>
+      </header>
+    </div>
+  );
+}
+export default App;
